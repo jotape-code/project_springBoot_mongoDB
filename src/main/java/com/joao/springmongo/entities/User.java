3861deb0df5fab7073b9418c071eb26ca53e2237
João pedro Floriano <joaopflori23@gmail.com>
2026-03-05 16:58:24 -0300

    referencing users posts

--- src/main/java/com/joao/springmongo/entities/User.java
+++ src/main/java/com/joao/springmongo/entities/User.java
@@ -1,8 +1,11 @@
 package com.joao.springmongo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -13,6 +16,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef
+    private List<Post> posts = new ArrayList<>();
 
     public User(){
     }
@@ -40,6 +46,9 @@ public class User implements Serializable {
     }
     public void setEmail(String email){
         this.email = email;
+    }
+    public List<Post> getPosts(){
+        return posts;
     }
 
     @Override
